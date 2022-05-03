2f39f8b987bc75e930dd9d6db88bb455a428e0fe
Preston Danielson <preston.danielson@gmail.com>
2022-05-03 12:34:50 -0500

    Initial commit (based on bike helmet impact monitor by jenfoxbot)

---
+++ ImpactSensor.py
@@ -0,0 +1,237 @@
+#!/usr/bin/python3
+
+#############################################
+#
+#   Helmet Guardian: Impact Force Monitor
+#
+#############################################
+# Code by jenfoxbot <jenfoxbot@gmail.com>
+# Code is open-source, coffee/beerware
+# Please keep header :)
+# If you like the content, consider
+#    buying me a coffee/beer if ya see me 
+#    or contributing to my patreon (jenfoxbot)
+#    to support projects like this! :D
+#############################################
+#
+# SO MANY THANKS to the wonderful folks who
+#    make & document libraries.
+#
+
+####################################
+# Libraries
+####################################
+#I2C library
+import smbus
+
+#GPIO
+import RPi.GPIO as GPIO
+
+#Other
+import math, time, os
+from signal import signal, SIGINT, SIGTERM
+from sys import exit
+
+####################################
+#        User Parameters
+#    (Edit these as necessary)
+####################################
+#Set LIS331 address
+addr = 0x19
+
+#Set the acceleration range
+maxScale = 24
+
+#Set the LED GPIO pin
+LED = 26
+
+#Open file to save all data
+#(creates new file in same folder if none
+#and appends to existing file)
+allData = open("AllSensorData.txt", "a")
+
+#Open file to save alert data
+#(creates new file in same folder if none
+#and appends to existing file)
+alrtData = open("AlertData.txt", "a")
+
+
+####################################
+# Initializations & Functions
+# (Leave as-is unless you are
+#   comfortable w/ code)
+####################################
+#LIS331 Constants (see Datasheet)
+CTRL_REG1 = 0x20
+CTRL_REG4 = 0x23
+OUT_X_L = 0x28
+OUT_X_H = 0x29
+OUT_Y_L = 0x2A
+OUT_Y_H = 0x2B
+OUT_Z_L = 0x2C
+OUT_Z_H = 0x2D
+
+POWERMODE_NORMAL = 0x27
+RANGE_6G = 0x00
+RANGE_12G = 0x10
+RANGE_24G = 0x30
+
+
+# Create I2C bus
+bus = smbus.SMBus(1)
+
+#Initialize GPIO and turn GPIO 26 to low
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED, GPIO.OUT)
+GPIO.output(LED, GPIO.LOW)
+
+#Initiliaze LIS331
+def initialize(addr, maxScale):
+    scale = int(maxScale)
+    #Initialize accelerometer control register 1: Normal Power Mode and 50 Hz sample rate
+    bus.write_byte_data(addr, CTRL_REG1, POWERMODE_NORMAL)
+    #Initialize acceleromter scale selection (6g, 12 g, or 24g). This example uses 24g
+    if maxScale == 6:
+        bus.write_byte_data(addr, CTRL_REG4, RANGE_6G)
+    elif maxScale == 12:
+        bus.write_byte_data(addr, CTRL_REG4, RANGE_12G)
+    elif maxScale == 24:
+        bus.write_byte_data(addr, CTRL_REG4, RANGE_24G)
+    else:
+        print("Error in the scale provided -- please enter 6, 12, or 24")
+
+
+#Function to read the data from accelerometer
+def readAxes(addr):
+    data0 = bus.read_byte_data(addr, OUT_X_L)
+    data1 = bus.read_byte_data(addr, OUT_X_H)
+    data2 = bus.read_byte_data(addr, OUT_Y_L)
+    data3 = bus.read_byte_data(addr, OUT_Y_H)
+    data4 = bus.read_byte_data(addr, OUT_Z_L)
+    data5 = bus.read_byte_data(addr, OUT_Z_H)
+    #Combine the two bytes and leftshit by 8
+    x = data0 | data1 << 8
+    y = data2 | data3 << 8
+    z = data4 | data5 << 8
+    #in case overflow
+    if x > 32767 :
+        x -= 65536
+    if y > 32767:
+        y -= 65536
+    if z > 32767 :
+        z -= 65536
+    #Calculate the two's complement as indicated in the datasheet
+    x = ~x
+    y = ~y
+    z = ~z
+    return x, y, z
+
+#Function to calculate g-force from acceleration data
+def convertToG(maxScale, xAccl, yAccl, zAccl):
+    #Caclulate "g" force based on the scale set by user
+    #Eqn: (2*range*reading)/totalBits (e.g. 48*reading/2^16)
+    X = (2*float(maxScale) * float(xAccl))/(2**16);
+    Y = (2*float(maxScale) * float(yAccl))/(2**16);
+    Z = (2*float(maxScale) * float(zAccl))/(2**16);
+    return X, Y, Z
+
+def isDanger(timestamp, x, y, z):
+    counter = 0
+
+    if abs(x) > 9 or abs(y) > 9 or abs(z) > 9:
+            alrtData.write(str(timestamp) + "\t" + "x: " + str(x) + "\t" + "y: " + str(y) + "\t" + "z: " +  str(z) + "\n")         
+            GPIO.output(LED, GPIO.HIGH)
+    elif abs(x) > 4 or abs(y) > 4 or abs(z) > 4:
+            while abs(x) > 4 or abs(y) > 4 or abs(z) > 4:
+                time_start = time.time()
+                counter = counter + 1
+                if counter > 4:
+                    break
+            time_end = time.time()
+            if (counter > 4):
+                alrtData.write(str(timestamp) + "\t" + "x: " + str(x) + "\t" + "y: " + str(y) + "\t" + "z: " + str(z) + "\n")
+                GPIO.output(LED, GPIO.HIGH)
+
+# closes files and GPIO
+def cleanup(signal_received, frame):
+        allData.close()
+        alrtData.close()
+        GPIO.cleanup()
+        exit(0)
+
+####################################
+#       Main Function
+####################################
+def main():
+    #Run this program unless there is a keyboard interrupt
+    signal(SIGINT, cleanup)
+    signal(SIGTERM, cleanup)
+    displayVal = 0
+    displayTime = 0
+    magnitude = 0
+    pr = 0
+
+    #print ("Starting stream")
+    while True:
+        #initialize LIS331 accelerometer
+        initialize(addr, 24)
+
+        #Start timestamp
+        ts = time.ctime()
+        
+        #Write timestamp to AllSensorData file 
+        allData.write(str(ts) + "\t")
+
+        #Get acceleration data for x, y, and z axes
+        xAccl, yAccl, zAccl = readAxes(addr)
+        #Calculate G force based on x, y, z acceleration data
+        x, y, z = convertToG(maxScale, xAccl, yAccl, zAccl)
+        #Determine if G force is dangerous to human body & take proper action (Update for martial arts applications)
+        #isDanger(ts, x, y, z)
+
+        #Write all sensor data to file AllSensorData (as you probably guessed :) )
+        allData.write("x: " + str(x) + "\t" + "y: " + str(y) + "\t" + "z: " + str(z) + "\n")
+
+        #If an increase in G-force is seen, capture temporarily. Reset after 5 seconds if no further increase is seen.
+        magnitude = math.sqrt((x**2) + (y**2) + (z**2))
+        
+        if magnitude > displayVal:
+            displayVal = magnitude
+            displayTime = time.time()
+
+        if magnitude > pr:
+            pr = magnitude
+        
+        if displayTime > 0:
+            elapsedTime = time.time() - displayTime
+            
+        if elapsedTime > 3:
+            displayVal = magnitude
+            displayTime = 0
+            elapsedTime = 0
+            
+
+        #Debug - print magnitude
+        #print("Magnitude : %.3f G" %magnitude)
+
+        #Debug - print all G values 
+        #print("Acceleration in X-Axis : %.3f" %x)
+        #print("Acceleration in Y-Axis : %.3f" %y)
+        #print("Acceleration in Z-Axis : %.3f" %z)
+        
+        #Debug - print accelerometer values
+        #print("X : %.3f" %xAccl)
+        #print("Y : %.3f" %yAccl)
+        #print("Z : %.3f" %zAccl)
+        
+        #print semi-colon delimited list of values for GUI (figure out how to normalize string length for better visuals)
+        print ("G-Force : %6.3f;Recent Maximum : %6.3f;Personal Record : %6.3f" % (magnitude, displayVal, pr))
+
+        #Debug - print elapsed time between recorded impacts
+        #print ("Elapsed time : %d" %elapsedTime)
+
+        #Short delay to prevent overclocking computer
+        time.sleep(0.05)
+
+if __name__ =="__main__":
+    main()
